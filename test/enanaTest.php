4bceb0c362a975571a89e5ce98ae4cb9d8f8a46a
KiksoS <quiquevolc@gmail.com>
2024-02-06 12:27:29 +0100

    test pocima sin tocar la funcion

--- test/enanaTest.php
+++ test/enanaTest.php
@@ -47,6 +47,10 @@ class EnanaTest extends TestCase {
     public function testPocimaRevive() {
         #Se probará el efecto de administrar una pócima a una Enana muerta pero con una vida mayor que -10 y menor que 0
         #Se tendrá que probar que la vida es mayor que 0 y que su situación ha cambiado a viva
+        $Francisca = new Enana("Francisca",-9);
+        $Francisca->pocima();
+        $this->assertEquals("viva", $Francisca->getSituacion());
+        $this->assertTrue($Pascualin->getPuntosVida() > 0);
 
     }
 
